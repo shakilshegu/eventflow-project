f378f343810d8c7b54a966f44b80b34323a874c2
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-20 11:14:48 +0530

    new commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,18 +1,21 @@
-import mongoose from 'mongoose'
-const MONGODB_URL = process.env.MONGODB_URL;
+import mongoose from "mongoose";
 
-let cached = (global as any).mongoose || { conn:null,promise:null};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-export const connectToDatabase = async ()=>{
-    if(cached.conn) return cached.conn;
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
-    if(!MONGODB_URL) throw new Error('MONGODB_URL is missing');
-    
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{
-        dbName:'Eventflow',
-        bufferCommands:false
-    })
-    cached.conn = await cached.promise;
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-    return cached.conn;
-}
+  if (!MONGODB_URI) throw new Error("MONGODB_URL is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "Eventflow",
+      bufferCommands: false,
+    });
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
