b7b43e0ccfe86bdbf853bd520d89045cdb20ac7c
shakilshagu <muhammadshakil1968@gmail.com>
2023-12-20 10:43:05 +0530

    new commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,22 +1,24 @@
 'use server'
 
-import { CreateUserParams, UpdateUserParams } from "@/types"
-import { handleError } from "../utils"
-import { connectToDatabase } from "../database"
-import User from "../database/models/user.model"
-import Order from "../database/models/order.model"
-import { revalidatePath } from "next/cache"
-import Event from "../database/models/event.model"
+import { revalidatePath } from 'next/cache'
 
-export const createUser = async (user:CreateUserParams)=>{
-    try{
-       await  connectToDatabase()
-       const newUser = await User.create(user);
-       return JSON.parse(JSON.stringify(newUser))
-    }catch(error){
-      handleError(error)  
-    }
-    
+import { connectToDatabase } from '@/lib/database'
+import User from '@/lib/database/models/user.model'
+import Order from '@/lib/database/models/order.model'
+import Event from '@/lib/database/models/event.model'
+import { handleError } from '@/lib/utils'
+
+import { CreateUserParams, UpdateUserParams } from '@/types'
+
+export async function createUser(user: CreateUserParams) {
+  try {
+    await connectToDatabase()
+
+    const newUser = await User.create(user)
+    return JSON.parse(JSON.stringify(newUser))
+  } catch (error) {
+    handleError(error)
+  }
 }
 
 export async function getUserById(userId: string) {
